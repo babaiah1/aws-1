fa987d528cc8106777765f150c3eb4972189fe2b
babaiah1 <144967993+babaiah1@users.noreply.github.com>
2025-08-10 15:56:33 -0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ecr_repo_name" {
+  default = "secure-ci-cd-demo"
+}
+
+variable "service_name" {
+  default = "secure-ci-cd-demo-service"
+}
